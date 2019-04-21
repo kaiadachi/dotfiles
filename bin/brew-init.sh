891e7d8f628132481bac0fd848078bb1d0f33755
kai adachi <kaikaikai8217@gmail.com>
2019-04-21 21:25:13 +0900

    chnage dir

--- bin/brew-init.sh
+++ bin/brew-init.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
+# #!/bin/bash
 source $(dirname $0)/.init.sh
 
+# brew install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# brew Applications install
 cd $PACKAGE_HOMEBREW_DIR && brew bundle dump
