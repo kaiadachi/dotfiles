011edfe4efd812bf841ef46e90d6910ca438e3f7
kai <36062615+kaiadachi@users.noreply.github.com>
2019-04-26 23:18:56 +0900

    Update brew-init.sh

--- bin/brew-init.sh
+++ bin/brew-init.sh
@@ -5,4 +5,4 @@ source $(dirname $0)/.init.sh
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # brew Applications install
-cd $PACKAGE_HOMEBREW_DIR && brew bundle dump
+cd $PACKAGE_HOMEBREW_DIR && brew bundle
